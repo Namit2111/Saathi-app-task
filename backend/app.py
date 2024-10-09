5d06c8d7516157c5be8050ddffee4f7d6a0b2532
Nmait2111 <namitjain2111@gmail.com>
2024-10-09 21:37:57 +0530

    universal port

--- backend/app.py
+++ backend/app.py
@@ -16,4 +16,4 @@ app = FastAPI(title="Recommendation System API")
 app.include_router(recommend.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
